32436b168f11bb17d2bd210bc7920ea4b8908d41
IvanFuschetto <133823780+IvanFuschetto@users.noreply.github.com>
2025-06-23 16:18:56 -0300

    Update desregistrar_usuario.sql

--- querys/desregistrar_usuario.sql
+++ querys/desregistrar_usuario.sql
@@ -1,2 +1,2 @@
 --Asumimos que existe el usuario con nombre_usuario = 'armando'
-DELETE FROM aplicacion_musica.usuarios WHERE nombre_usuario = 'vale_rock'
+DELETE FROM aplicacion_musica.usuarios WHERE nombre_usuario = 'vale_rock';

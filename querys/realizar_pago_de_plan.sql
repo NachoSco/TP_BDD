5318303d0e374fb382015638b6ef445720900352
IvanFuschetto <133823780+IvanFuschetto@users.noreply.github.com>
2025-06-23 15:48:45 -0300

    Update realizar_pago_de_plan.sql

--- querys/realizar_pago_de_plan.sql
+++ querys/realizar_pago_de_plan.sql
@@ -1,3 +1,3 @@
 --Asumimos que existe el Plan De subscripcionm Premium y su precio es de 3000
-INSERT INTO pagos (nombre_usuario, nombre_plan, forma_pago, precio_plan) VALUES
+INSERT INTO aplicacion_musica.pagos (nombre_usuario, nombre_plan, forma_pago, precio_plan) VALUES
 ('vale_rock', 'Premium', 'tarjeta',3000);

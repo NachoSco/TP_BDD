c81769ccc648e82b1fa0227c393cbc73c95d64ad
IvanFuschetto <133823780+IvanFuschetto@users.noreply.github.com>
2025-06-23 15:20:10 -0300

    Update listar_album_por_artista.sql

--- querys/listar_album_por_artista.sql
+++ querys/listar_album_por_artista.sql
@@ -1,1 +1,7 @@
-SELECT * FROM albumes GROUP BY nombre_artista;
+SELECT *
+FROM aplicacion_musica.albumes 
+ORDER BY nombre_artista;
+--OPCION 2
+--SELECT nombre_artista,nombre_album,fecha_lanzamiento FROM aplicacion_musica.albumes GROUP BY nombre_artista,nombre_album,fecha_lanzamiento;
+--OPCION 3
+--SELECT * FROM aplicacion_musica.albumes;
